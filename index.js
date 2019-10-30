4ff1700624528dcb175f990d26a7dcfacf4402bc
dnyceboy <55847687+dnyceboy@users.noreply.github.com>
2019-10-30 13:45:50 -0400

    Update index.js
    Forgot ? for variable prompt line 4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var answer = prompt("Do You Love Me?");
 
 while (answer.indexOf("yes") === -1 &&answer.indexOf("yeah") === -1) {
-    var answer = prompt("Do You Love Me");
+    var answer = prompt("Do You Love Me?");
 }
 
 alert("YAY I lOVE YOU TOO!!! XD");
